3720840bfbd991b9209792447db61a62c564cd12
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2023-06-05 14:21:22 +0600

    Add new reducers

--- pustokio-store/slice/auth/index.ts
+++ pustokio-store/slice/auth/index.ts
@@ -1,23 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { HYDRATE } from "next-redux-wrapper";
-// import { AnyAction } from "redux";
-// import {
-//   AUTH_SUCCESS,
-//   AUTH_TOKEN_REFRESH,
-//   AUTH_LOGOUT,
-//   AUTH_START,
-//   AUTH_FAIL,
-//   AUTH_REDIRECT_SUCCESS,
-//   AUTH_REDIRECT_START,
-//   AUTH_ERROR_REFRESH,
-//   AUTH_CHECKING_STATE_END,
-//   FORGOT_PASS_START,
-//   FORGOT_PASS_SUCCESS,
-//   FORGOT_PASS_FAIL,
-//   RESET_PASS_START,
-//   RESET_PASS_SUCCESS,
-//   RESET_PASS_FAIL,
-// } from "../../actions/actionTypes";
 
 export interface AuthState {
   accessToken: string | null;
@@ -35,7 +16,7 @@ export interface AuthState {
   resetPassErr: { message: string; status: number } | null;
 }
 
-export const initialState = {
+export const initialState: AuthState = {
   accessToken: null,
   userId: null,
   expirationDate: 0,
@@ -56,80 +37,67 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     authSuccess: (state, action) => {
-      state.accessToken = action.payload.accessToken;
-      state.userId = action.payload.userId;
-      state.expirationDate = action.payload.expirationDate;
+      const { accessToken, userId, expirationDate } = action.payload;
+
+      state.accessToken = accessToken;
+      state.userId = userId;
+      state.expirationDate = expirationDate;
       state.loading = false;
       state.error = null;
+    },
+    authStart: (state, _action) => {
+      state.loading = true;
+      state.error = null;
+    },
+    authTokenRefresh(state, action) {
+      const { accessToken, expirationDate } = action.payload;
+      state.accessToken = accessToken;
+      state.expirationDate = expirationDate;
+    },
+    authFail(state, action) {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+    authRedirectStart(state, action) {
+      state.authRedirectPath = action.payload.authRedirectPath;
+    },
+    authRedirectSuccess(state, _action) {
+      state.authRedirectPath = null;
+    },
+    authLogout(state, _action) {
+      state.authRedirectPath = "/";
+      state.authCheckingState = false;
+    },
+    authErrorRefresh(state, _action) {
+      state.error = null;
+    },
+    authForgotPassStart(state, _action) {
+      state.forgotPassLoading = true;
+    },
+    authForgotPassSuccess(state, action) {
+      state.forgotPassLoading = false;
+      state.forgotPassMsg = action.payload.forgotPassMsg;
+    },
+    authForgotPassFail(state, action) {
+      state.forgotPassLoading = false;
+      state.forgotPassErr = action.payload.forgotPassErr;
+    },
+    authResetPassStart(state, _action) {
+      state.resetPassLoading = true;
+    },
+    authResetPassSuccess(state, action) {
+      state.resetPassLoading = false;
+      state.resetPassMsg = action.payload.resetPassMsg;
+    },
+    authResetPassFail(state, action) {
+      state.resetPassLoading = false;
+      state.resetPassErr = action.payload.resetPassErr;
+    },
+    authCheckingStateEnd(state, _action) {
+      state.authCheckingState = false;
     },
   },
 });
 
 export const { authSuccess } = authSlice.actions;
 export default authSlice.reducer;
-
-// const reducer = (state = initialState, action: AnyAction) => {
-//   const {
-//     accessToken,
-//     userId,
-//     expirationDate,
-//     error,
-//     authRedirectPath,
-//     forgotPassMsg,
-//     forgotPassErr,
-//     resetPassMsg,
-//     resetPassErr,
-//   } = action;
-
-//   switch (action.type) {
-//     case HYDRATE:
-//       return { ...state };
-//     case AUTH_START:
-//       return { ...state, loading: true, error: null };
-//     case AUTH_SUCCESS:
-//       return {
-//         ...state,
-//         accessToken,
-//         userId,
-//         expirationDate,
-//         loading: false,
-//         error: null,
-//       };
-//     case AUTH_TOKEN_REFRESH:
-//       return { ...state, accessToken, expirationDate };
-//     case AUTH_FAIL:
-//       return { ...state, loading: false, error };
-//     case AUTH_REDIRECT_START:
-//       return { ...state, authRedirectPath };
-//     case AUTH_REDIRECT_SUCCESS:
-//       return { ...state, authRedirectPath: null };
-//     case AUTH_LOGOUT:
-//       return {
-//         ...state,
-//         authRedirectPath: "/",
-//         authCheckingState: false,
-//       };
-//     case AUTH_ERROR_REFRESH:
-//       return { ...state, error: null };
-//     case FORGOT_PASS_START:
-//       return { ...state, forgotPassLoading: true };
-//     case FORGOT_PASS_SUCCESS:
-//       return { ...state, forgotPassLoading: false, forgotPassMsg };
-//     case FORGOT_PASS_FAIL:
-//       return { ...state, forgotPassLoading: false, forgotPassErr };
-//     case RESET_PASS_START:
-//       return { ...state, resetPassLoading: true };
-//     case RESET_PASS_SUCCESS:
-//       return { ...state, resetPassLoading: false, resetPassMsg };
-//     case RESET_PASS_FAIL:
-//       return { ...state, resetPassLoading: false, resetPassErr };
-//     case AUTH_CHECKING_STATE_END: {
-//       return { ...state, authCheckingState: false };
-//     }
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export default reducer;
